54e53e5a4b94d0e23064f59ca783b5e3c0a4d6f4
nniikon <kolya.kasparov@gmail.com>
2024-10-23 02:32:05 +0300

    add logger

--- photoshop/source/main.cpp
+++ photoshop/source/main.cpp
@@ -1,9 +1,13 @@
 #include "api_sfm.hpp"
 #include "sfm_implementation.h"
 
+#include "loguru.hpp"
+
 using namespace psapi;
 
-int main() {
+int main(int argc, char *argv[]) {
+    loguru::init(argc, argv);
+    LOG_F(INFO, "Photoshop started!");
 
     sfm::RenderWindow window(800, 600, "StandPhotoshop");
 
