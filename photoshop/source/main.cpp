c25978b96b567cf27c2da69fc785a257b9c0d3bf
nniikon <kolya.kasparov@gmail.com>
2024-10-30 04:35:02 +0300

    update main to load plugins

--- photoshop/source/main.cpp
+++ photoshop/source/main.cpp
@@ -1,32 +1,75 @@
+#include <dlfcn.h>
+#include <immintrin.h>
+
 #include "api_sfm.hpp"
 #include "sfm_implementation.h"
-#include "ps_plugin_toolbar.h"
+#include "api_photoshop.hpp"
 
 #include "loguru.hpp"
-#include <dlfcn.h>
 
 using namespace psapi;
 
+const std::string kPluginPaths[] = {
+    "./system_plugins/libplugin_toolbar.so",
+    "./system_plugins/libplugin_canvas.so",
+    "./system_plugins/libplugin_brush.so",
+    "./system_plugins/libplugin_line.so",
+};
+
+int loadPlugins();
+
 int main(int argc, char *argv[]) {
     loguru::init(argc, argv);
-    LOG_F(INFO, "Photoshop started!");
 
-    sfm::RenderWindow window(800, 600, "StandPhotoshop");
+    sfm::RenderWindow window(1200, 1080, "Photoshop");
 
-    loadPlugin();
+    int err = loadPlugins();
+    if (err) {
+        LOG_F(ERROR, "Can't load plugins");
+        return 1;
+    }
 
     while (window.isOpen()) {
+        uint64_t time1 = __rdtsc();
         sfm::Event event;
-        if (window.pollEvent(event)) {
+        while (window.pollEvent(event)) {
             if (event.type == sfm::Event::Closed) {
                 window.close();
             }
+            getRootWindow()->update(&window, event);
         }
+
+        getRootWindow()->update(&window, {.type = sfm::Event::None});
+        getRootWindow()->draw(&window);
+
+        uint64_t time2 = __rdtsc();
         window.display();
         window.clear();
+        uint64_t time3 = __rdtsc();
 
-        getRootWindow()->draw(&window);
-        getRootWindow()->update(&window, event);
+
+        // LOG_F(INFO, "Business: %f%%", float(time2 - time1) / float(time3 - time1) * 100.f);
+    }
+
+    return 0;
+}
+
+int loadPlugins() {
+    for (const auto& path : kPluginPaths) {
+        void* lib = dlopen(path.c_str(), RTLD_LAZY);
+        if (!lib) {
+            LOG_F(ERROR, "Can't load plugin: %s", dlerror());
+            return 1;
+        }
+
+        auto loadPlugin = (bool (*)(void))dlsym(lib, "loadPlugin");
+        if (!loadPlugin) {
+
+            LOG_F(ERROR, "Can't load plugin: %s", dlerror());
+            return 1;
+        }
+
+        loadPlugin();
     }
 
     return 0;
