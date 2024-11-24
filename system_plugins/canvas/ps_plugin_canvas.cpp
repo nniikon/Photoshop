5f1fa2081983d8f4f7097376b1b98d4bbc6209d5
nniikon <kolya.kasparov@gmail.com>
2024-11-24 14:43:19 +0300

    make canvas plugin scrollable

--- system_plugins/canvas/ps_plugin_canvas.cpp
+++ system_plugins/canvas/ps_plugin_canvas.cpp
@@ -1,13 +1,14 @@
 #include "ps_plugin_canvas.h"
 
+#include "loguru.hpp"
+
 #include <memory>
 
-#include "ps_canvas.h"
-
 bool loadPlugin() {
-    std::unique_ptr<psapi::ICanvas> canvas = std::make_unique<ps::Canvas>(
-            psapi::vec2i{600, 950},
-            psapi::vec2i{300, 50}
+    std::unique_ptr<psapi::ICanvas> canvas 
+                = std::make_unique<ps::ScrollableDecorator<ps::Canvas>>(
+        psapi::vec2i{600, 950},
+        psapi::vec2i{300, 50}
     );
 
     psapi::getRootWindow()->addWindow(std::move(canvas));
